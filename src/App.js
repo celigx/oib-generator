345b4f50f68d87a1868f55d50482240d8a71b612
Celig <cela@protonmail.com>
2021-09-03 16:21:55 +0200

    Add random 10 digit number

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import './App.sass';
 
 function App() {
+  const [number, setNumber] = useState(Math.floor(Math.random() * 9000000000) + 1000000000)
+  
+  // Create random 10 digit number
+  const randomNumber = () => {
+    setNumber(Math.floor(Math.random() * 9000000000) + 1000000000)
+  } 
+
+
   return (
     <div className="app">
 
