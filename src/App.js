79cb535983aa8d8135fc2843287d7f0b8665d891
Celig <cela@protonmail.com>
2021-09-06 23:26:12 +0200

    Remove tooltip message on mobile and tablet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.sass';
 
 function App() {
   const [number, setNumber] = useState(Math.floor(Math.random() * 9000000000) + 1000000000)
   const [copyMessage, setCopyMessage] = useState('Copy!')
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowSize)
+    }
+  }, [])
+
+  let isMobile = (width <= 768)
+
+  const handleWindowSize = () => {
+    setWidth(window.innerWidth)
+  }
   
   // Create random 10 digit number
   const randomNumber = () => {
@@ -32,6 +47,7 @@ function App() {
       console.log('ostatak:', remainder);
     }
 
+    // Calculate control number
     const control = 11 - remainder === 10 ? 0 : 11 - remainder
     console.log('kontrolniBroj:', control);
     return control
@@ -42,8 +58,13 @@ function App() {
   console.log(OIB);
 
   const copyToClipboard = () => {
-    navigator.clipboard.writeText(OIB)
-    setCopyMessage('Copied!')
+    if (isMobile) {
+      return null
+    } else {
+      // Copy number to clipboard on click
+      navigator.clipboard.writeText(OIB)
+      setCopyMessage('Copied!')
+    }
   }
 
   const changeMessage = () => {
@@ -51,11 +72,11 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <h1 className="title">OIB Generator</h1>
-      <h1 className="oib tooltip" onClick={() => copyToClipboard()} onMouseLeave={changeMessage}>{OIB} <span className="tooltipText">{copyMessage}</span></h1>
-      <button className="button" onClick={() => randomNumber()}>Generiraj novi OIB</button>
-    </div>
+      <div className="app">
+        <h1 className="title">OIB Generator</h1>
+        <h1 className="oib tooltip" onClick={() => copyToClipboard()} onMouseLeave={changeMessage}>{OIB} <span className="tooltipText">{copyMessage}</span></h1>
+        <button className="button" onClick={() => randomNumber()}>Generiraj novi OIB</button>
+      </div>
   );
 }
 
